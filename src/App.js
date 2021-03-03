e393f7494314e0c591ae8e507681a349ce1cb5ad
Joshua-Salvador <424joshuae.b.s@gmail.com>
2021-03-03 20:27:50 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CoverLetter from "./components/CoverLetter";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import RespondentInformation from "./components/RespondentInformation";
+import Questions from "./components/Questions";
+import Feedback from "./components/Feedback";
+import { useSelector } from "react-redux";
+import ThankYou from "./components/ThankYou";
+import DataVisuals from "./components/DataVisuals";
 
 function App() {
+  const state = useSelector((state) => state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {state.email ? <Redirect to="/resinfo" /> : <CoverLetter />}
+          </Route>
+          <Route exact path="/resinfo">
+            {state.email ? <RespondentInformation /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/questions">
+            {localStorage.getItem("uid") ? <Questions /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/feedback">
+            {state.email ? <Feedback /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/thankyou">
+            {state.email ? <ThankYou /> : <Redirect to="/" />}
+          </Route>
+          <Route exact path="/vizdata">
+            <DataVisuals />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux";
+import { ThemeProvider } from "@material-ui/core";
+import theme from "./MuiTheme";
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcfO7vs9q4xCkXjaFXv1byKWKVB8YlBbE",
+  authDomain: "usc-shs-research02.firebaseapp.com",
+  projectId: "usc-shs-research02",
+  storageBucket: "usc-shs-research02.appspot.com",
+  messagingSenderId: "1068215553841",
+  appId: "1:1068215553841:web:d12813330a326c00572a10",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
